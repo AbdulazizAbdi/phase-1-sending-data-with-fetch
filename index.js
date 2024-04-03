8dc0b54f4d0e68cc4b7d2c7b62f24fc91be63681
Abdulaziz Abdi <amigf76@gmail.com>
2024-04-03 15:40:06 +0300

    sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userMail){
+    return fetch('http://localhost:3000/users', {
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+        },
+        body : JSON.stringify({
+            name : userName,
+            email : userMail
+        })
+    })
+    .then(response => response.json())
+    .then(userObject => {
+        const idHeader = document.createElement('h2')
+        idHeader.textContent = userObject.id;
+        console.log(idHeader)
+        document.querySelector('body').appendChild(idHeader)
+    })
+    .catch(error => {
+        alert("ERROR CAUGHT!")
+        const messageForError = document.createElement('h2');
+        messageForError.textContent = error.message;
+        document.querySelector('body').appendChild(messageForError)
+    })
+}
